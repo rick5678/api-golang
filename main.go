5e4ac508c54f4a95f45f027f3ef6aeb33d28c55f
Henrique Franco <henriquefrancoaraujo@gmail.com>
2022-08-25 14:25:32 -0300

    fist commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"main/configs"
+	"main/handlers"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	err := configs.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Post("/", handlers.Create)
+	r.Put("/{id]", handlers.Update)
+	r.Delete("/{id", handlers.Delete)
+	r.Get("/", handlers.List)
+	r.Get("/{id}", handlers.Get)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
+
+	return
+}
